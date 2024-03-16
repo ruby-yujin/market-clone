01b9c1c7aef7308ffe5a8ed0e273dd7f97ff2f3a
yujin Park <101633842+ruby-yujin@users.noreply.github.com>
2024-03-17 00:38:16 +0900

    [feature] firebase + wirte page 연결

--- src/pages/Main.svelte
+++ src/pages/Main.svelte
@@ -1,3 +1,5 @@
+
+
 <div class="mediaquery-dim">화면 사이즈를 줄여주세요</div>
 
 <div class="outline">
--- src/pages/Write.svelte
+++ src/pages/Write.svelte
@@ -1,28 +1,49 @@
+<script>
+import { getDatabase, ref, push } from "firebase/database";
+
+let title
+let price
+let description
+let place
+
+function writeUserData() {
+  console.log('호출');
+  const db = getDatabase();
+  push(ref(db, 'items/'+ title), {
+    title,
+    price,
+    description,
+    place, 
+  });
+
+}
+
+</script>
 <main class="write-main">
     <h1>글쓰기</h1>
-    <form id="write-form">
-      <div class="wirte-block">
+    <form id="write-form" on:submit|preventDefault={writeUserData}>
+      <!-- <div class="wirte-block">
         <label for="image">이미지</label>
         <input type="file" id="image" name="image" />
-      </div>
+      </div> -->
       <div class="wirte-block">
         <label for="title">제목</label>
-        <input type="text" id="title" name="title" />
+        <input type="text" id="title" name="title" bind:value={title}/>
       </div>
       <div class="wirte-block">
         <label for="price">가격</label>
-        <input type="number" id="price" name="price" />
+        <input type="number" id="price" name="price" bind:value={price}/>
       </div>
       <div class="wirte-block">
         <label for="description">설명</label>
-        <input type="text" id="description" name="description" />
+        <input type="text" id="description" name="description"  bind:value={description} />
       </div>
       <div class="wirte-block">
         <label for="place">장소</label>
-        <input type="text" id="place" name="place" />
+        <input type="text" id="place" name="place" bind:value={place} />
       </div>
       <div class="wirte-block">
-        <button type="submit" id="btn-submit" class="btn-default">
+        <button type="submit" id="btn-submit" class="btn-default" >
           제출
         </button>
       </div>
